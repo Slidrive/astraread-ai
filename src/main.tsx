c6170d3d9b174af57e52ef9798aa0d0f17365ebe
SkiDrive <154932592+Slidrive@users.noreply.github.com>
2025-12-03 02:14:28 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import { ErrorBoundary } from "react-error-boundary";
-import "@github/spark/spark"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './main.css';
 
-import App from './App.tsx'
-import { ErrorFallback } from './ErrorFallback.tsx'
-
-import "./main.css"
-
-createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-   </ErrorBoundary>
-)
+  </React.StrictMode>
+);
